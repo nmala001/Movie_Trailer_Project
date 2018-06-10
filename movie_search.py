30d11ce08850a4ec34d9509756b1ed1e3bc1384d
mgunn001 <nmala001@odu.edu>
2018-06-10 19:21:33 -0400

    Adjusted movie_search.py to pep8

--- movie_search.py
+++ movie_search.py
@@ -5,27 +5,23 @@
 import requests
 from googleapiclient.discovery import build
 
-
-
 # We will use API keys saved in config.py file
-
 
 YOUTUBE_API_KEY = 'AIzaSyAsLq6Y7_ccreQK3omR-i1zjTMuQGv2XZI'
 OMDB_API_KEY = 'cb657e5c'
 
+
 def _convert_to_lower(dictionary):
 
-    #A helper method which converts all the keys to lower case
-
-    # We use it in the omdb_search to convert the in the API response dictionary to lowercase before the function returns
-
+    # A helper method which converts all the keys to lower case
+    """
+    We use it in the omdb_search to convert the in the API response
+    dictionary to lowercase before the function returns
+    """
     """
     Args: dictionary (dict): A dictionary
-
     Returns: The dictionary with the lower case keys
-
     """
-
     lower_case_dictionary = dict()
 
     for key in dictionary:
@@ -35,36 +31,38 @@ def _convert_to_lower(dictionary):
     return lower_case_dictionary
 
 
-
 def youtube_video_id(title, *args):
-
     """
       The Youtube service object is built and API calls are made
 
       -We use Google API client library in python and a YOUTUBE API key
        to make API calls to search for the movie trailers.
 
-        -We use the build service object to make API calls specific to movie trailers(see the 'payload' variable below)
+        -We use the build service object to make API calls
+        specific to movie trailers(see the 'payload' variable below)
 
     Args:
         title(str): The title of the movie to search for
-        *args(str or None): More words to add to query like the Movie release year
+        *args(str or None): More words to add to query
+        like the Movie release year
 
     Returns:
-        str: The youtube video ID that corresponds to top search result.
+        str: The youtube video ID that corresponds
+        to top search result.
 
     References:
         - https://developers.google.com/youtube/v3/docs/search/list
         - https://developers.google.com/api-client-library/python
     """
-
-    youtube = build('youtube', 'v3', developerKey = YOUTUBE_API_KEY)
+    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
     query = '+'.join(['trailer', title] + [x for x in args if x is not None])
 
-    payload = dict(q=query, part="id", maxResults=1,type='video', topicId='/m/02vxn')
+    payload = dict(q=query, part="id", maxResults=1,
+                   type='video', topicId='/m/02vxn')
 
-    #Calling the search.list method to call the results matching the query term
+    # Calling the search.list method to call
+    # the results matching the query term
 
     search = youtube.search().list(**payload)
     response = search.execute()
@@ -73,12 +71,14 @@ def youtube_video_id(title, *args):
 
     return str(video_id)
 
+
 def omdb_movie_info(title, year=None):
 
     """
       -It places the calls to omdbapi.com using the python request packages.
 
-      - We use this function to create a request payload if a valid OMDB API key is given using the request library.
+      - We use this function to create a request payload
+        if a valid OMDB API key is given using the request library.
 
       - It places the GET request to omdbapi.com
 
@@ -98,14 +98,11 @@ def omdb_movie_info(title, year=None):
 
     omdb_url = 'http://www.omdbapi.com/'
 
-    #Referred omdbapi.com for details on query parameters
+    # Referred omdbapi.com for details on query parameters
 
-    payload = dict(apikey=OMDB_API_KEY, type= 'movie', t=title,y=year)
+    payload = dict(apikey=OMDB_API_KEY, type='movie', t=title, y=year)
 
     response = requests.get(omdb_url, params=payload)
     response.raise_for_status()
 
     return _convert_to_lower(response.json())
-
-
-
